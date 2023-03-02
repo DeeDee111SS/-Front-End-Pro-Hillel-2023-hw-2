27e4b194806e908378a423375b55b64d548c5f97
Dmitrij Duvanov <you@example.com>
2023-03-02 17:22:11 +0200

    fixed again

--- script/script.js
+++ script/script.js
@@ -1,27 +1,51 @@
 
 let a = prompt('Enter the first number');
-let operator = prompt('Enter the operator');
 let b = prompt('Enter the second number');
 
 
-function calculate(a, b, operator) {
-    
-    if (operator == "+") {
-        console.log('Result: ' + a + operator + b + ' = ' + (Number(a) + Number(b)));
-        
-    } else if (operator == "-") {
-        console.log('Result: ' + a + operator + b + ' = ' + (a - b));
-        
-    } else if (operator == "*") {
-        console.log('Result: ' + a + operator + b + ' = ' + (a * b));
-        
-    } else if (operator == "/") {
-        console.log('Result: ' + a + operator + b + ' = ' + (a / b));
-        
-    } else {
-        console.log("uncorrect input data");
-    }     
+function add (a, b) {
+    console.log('Result: ' + a + ' + ' + b + ' = ' + (Number(a) + Number(b)));
 }
 
-calculate(a, b, operator);
+function sub (a, b) {
+    console.log('Result: ' + a + ' - ' + b + ' = ' + (a - b));
+}
+
+function mult (a, b) {
+    console.log('Result: ' + a + ' * ' + b + ' = ' + (a * b));
+}
+
+function div (a, b) {
+    console.log('Result: ' + a + ' / ' + b + ' = ' + (a / b));
+}
+
+add(a, b);
+sub(a, b);
+mult(a, b);
+div(a, b);
+
+
+
+// let operator = prompt('Enter the operator');
+
+// function calculate(a, b, operator) {
+    
+//     if (operator == "+") {
+//         console.log('Result: ' + a + operator + b + ' = ' + (Number(a) + Number(b)));
+        
+//     } else if (operator == "-") {
+//         console.log('Result: ' + a + operator + b + ' = ' + (a - b));
+        
+//     } else if (operator == "*") {
+//         console.log('Result: ' + a + operator + b + ' = ' + (a * b));
+        
+//     } else if (operator == "/") {
+//         console.log('Result: ' + a + operator + b + ' = ' + (a / b));
+        
+//     } else {
+//         console.log("uncorrect input data");
+//     }     
+// }
+
+// calculate(a, b, operator);
 
